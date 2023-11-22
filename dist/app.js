0479a7b3e37898f577774b1a03e783a9f9e9956a
Jaewoong <dixk3458@naver.com>
2023-11-22 17:28:28 +0900

    Image Component 생성

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,11 @@
-import { PageCompoent } from './components/page.js';
+import { ImageComponent } from './components/page/item/image.js';
+import { PageCompoent } from './components/page/page.js';
 class App {
     constructor(appRoot) {
         this.page = new PageCompoent();
         this.page.attachTo(appRoot);
+        const image = new ImageComponent('imageComponent', 'https://images.unsplash.com/photo-1700335739138-150bf313be41?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8');
+        image.attachTo(appRoot, 'beforebegin');
     }
 }
 new App(document.querySelector('.document'));
